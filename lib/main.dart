763a9288765df524aede6479a2b337fb92c7bd84
John Visto <tapotsiv@gmail.com>
2020-04-28 07:36:37 +0800

    April 28, 2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -31,29 +31,66 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  int trackScore = 0;
+
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
+    void trackScoreNew() {
+      if (userPickedAnswer == correctAnswer) {
+        trackScore++;
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+    }
+
     setState(() {
       //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
-      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-      //HINT! Step 4 Part B is in the quiz_brain.dart
-      //TODO: Step 4 Part C - reset the questionNumber,
-      //TODO: Step 4 Part D - empty out the scoreKeeper.
+      if (quizBrain.isFinished() == true) {
+        trackScoreNew();
+        //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+        Alert(
+          context: context,
+          type: AlertType.success,
+          title: "Quiz Complete!",
+          desc: "You\'re total score is $trackScore.",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Start Over",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              width: 120,
+            )
+          ],
+        ).show();
+
+        //TODO: Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO: Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+
+        trackScore = 0;
+      }
 
       //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      if (userPickedAnswer == correctAnswer) {
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
-      } else {
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
+      else {
+        trackScoreNew();
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
     });
   }
 
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 import 'question.dart';
 
 class QuizBrain {
@@ -37,6 +39,7 @@ class QuizBrain {
     }
   }
 
+
   String getQuestionText() {
     return _questionBank[_questionNumber].questionText;
   }
@@ -45,9 +48,21 @@ class QuizBrain {
     return _questionBank[_questionNumber].questionAnswer;
   }
 
+
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1){
+      print('You\'re on the last question');
+      return true;
+    } else {
+      return false;
+    }
 
-  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+      //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of    the quiz and when a restart should happen.
 
+  }
   //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+  }
 }
